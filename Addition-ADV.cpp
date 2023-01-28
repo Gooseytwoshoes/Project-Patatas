a7bb08d3ca0208d0d9ba3b2428375b786fc92afc
Francis Villanueva <83599617+Gooseytwoshoes@users.noreply.github.com>
2023-01-28 18:11:46 +0800

    1.00v
    fetch user input to add how many numbers the user wants

---
+++ Addition-ADV.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int max, sum, i, j;
+
+    cout << "How many numbers do you want to enter? ";
+    cin >> max;
+      //provide program to have a limit on how many to add
+    
+    int arr[max] = {};
+      //store values given by the user in respect to how many the user want to add
+
+    for (i = 0; i < max; i++)
+    {
+        cout << "Enter number "<< i + 1 << " :";
+        cin >> arr[i];
+          //put the value into the array "arr"
+    }
+
+    for (j = 0; j < max; j++){
+        sum = sum + arr[j];  // add values of the array one by one
+    }
+    
+
+    return 0;
+}
